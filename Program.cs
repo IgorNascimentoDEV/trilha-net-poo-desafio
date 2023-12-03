cd0a28607cb1f2f3bc514068b503166be651d770
Igor Nascimento <103793795+IgorNascimentoDEV@users.noreply.github.com>
2023-12-02 22:34:00 -0300

    Finalizando desafio da herança

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia Model", "IMEI123", 64);
+Smartphone iphone = new Iphone("987654321", "iPhone Model", "IMEI456", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
